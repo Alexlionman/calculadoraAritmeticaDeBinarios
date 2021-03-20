6cd13d1763c95477c2cd36d8d56848441c890646
Alexlionman <pereirasander33@gmail.com>
2021-03-19 21:53:02 -0300

    Desacoplamentos das funções

--- Principal/calculadora.py
+++ Principal/calculadora.py
@@ -1,17 +1,79 @@
-#Conversão de binário para inteiro int("numeroEmBinario", 2)
-
-valorEmDecimal1 = int(input("Insira o valor:"), 2)
-valorEmDecimal2 = int(input("Insira o valor:"), 2)
-print("Escolha a operação: \n 1 - Soma \n 2 - Multiplicação \n 3 - Divisão")
-valorSelecionado = input("Insira o numero da operação desejada: ")
-
-if valorSelecionado == "1":
-  print("Resultado da soma", valorEmDecimal1 + valorEmDecimal2)
-elif valorSelecionado == "2":
-    print("Resultado da multiplicação: ", valorEmDecimal1 * valorEmDecimal2)
-elif valorSelecionado == "3":
-    print("Resultado da divisão: ", valorEmDecimal1 // valorEmDecimal2)
-else:
-    print("Selecione uma das 3 opções")
+# Conversão de binário para inteiro int("numeroEmBinario", 2)
+from time import sleep
 
 
+def funcaoEscritaInicial():
+    print('---CALCULADORA DE BINÁRIOS---')
+    sleep(0.5)
+    print('|||||||||||||||       |||||||||||||||       |||||||||||||||')
+    print('|||         |||       |||         |||       |||            ')
+    sleep(0.5)
+    print('|||         |||       |||         |||       |||            ')
+    print('|||         |||       |||         |||       |||            ')
+    sleep(0.5)
+    print('|||         |||       |||         |||       |||            ')
+    print('|||||||||||||||       |||||||||||||||       |||            ')
+    sleep(0.5)
+    print('|||||||||||||||       |||||||||||||||       |||            ')
+    print('|||         |||       |||         |||       |||            ')
+    sleep(0.5)
+    print('|||         |||       |||         |||       |||            ')
+    print('|||         |||       |||         |||       |||            ')
+    sleep(0.5)
+    print('|||         |||       |||         |||       |||            ')
+    print('|||         |||       |||         |||       |||||||||||||||')
+    print('\n\n')
+    sleep(0.5)
+    print('Seja bem vindo a calculadora, vamos começar?')
+    sleep(1)
+
+
+def separador():
+    print('----------------------------------------------------------')
+
+
+def obtencaoDosValores():
+    valorEmDecimal1 = int(input("Insira o 1º valor:"), 2)
+    sleep(0.3)
+    print("...")
+    valorEmDecimal2 = int(input("Agora insira o 2º valor:"), 2)
+    sleep(0.5)
+    separador()
+    funcaoPrincipal(valorEmDecimal1, valorEmDecimal2)
+
+
+def funcaoPrincipal(valor1, valor2):
+    print("Qual operação você deseja realizar?\n\
+        Digite + para somar \n\
+        Digite * para multiplicação\n\
+        Digite / para divisão\n\
+        Digite s para sair")
+    valorSelecionado = input("Insira o numero da operação desejada: ")
+    if valorSelecionado == "+":
+        print("O Resultado da soma", valor1 + valor2)
+    elif valorSelecionado == "*":
+        print("O Resultado da multiplicação: ",
+              valor1 * valor2)
+    elif valorSelecionado == "/":
+        print("O Resultado da divisão: ", valor1 // valor2)
+    elif valorSelecionado == "s":
+        sleep(0.7)
+        print('Até a próxima...')
+        exit()
+    else:
+        print("""Você precisa selecionar uma das opções mostradas, vamos tentar
+         novamente.""")
+        sleep(0.6)
+    funcaoPrincipal(valor1, valor2)
+
+
+'''Funcao para validação dos caracteres digitados
+def testeNumeros():
+    nossaString = "ahah"
+    for i in list(nossaString):
+        if not i == "a":
+            print('a')'''
+
+
+funcaoEscritaInicial()
+obtencaoDosValores()
